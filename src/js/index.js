481f64baaac0520c6bd3c1cf2240e8ac4b8fcfc3
Matheus Araujo <matheus.kyuubinoz@gmail.com>
2022-04-06 01:52:23 -0300

    estrutura html

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(){
+        this.id = "";
+
+    }
+    add(){
+        alert("produto  adicionado")
+    }
+    reset(){
+        alert("limpo")
+    }
+}
+
+let produto = new Product
